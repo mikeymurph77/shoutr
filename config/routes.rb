3661dae21ee13a4e2b1f210e8d93b660133f036c
Mike Murphy <mikeymurph77@gmail.com>
2014-06-30 13:29:02 -0400

    Add user feed and logo image

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   require "monban/constraints/signed_in"
 
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :shouts, only: [:create]
 
   get "/sign_up", to: "users#new"
